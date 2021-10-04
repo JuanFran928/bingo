2ba8b0313ce07be2bab8561f20a91b63d2383f3e
Juan Fran Martin <juanframaro@gmail.com>
2021-10-04 23:30:44 +0100

    renamed variables in index.js and changed some css

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,21 @@
-const getNumberButton = document.getElementById("getNumberButton");
-const startGameButton = document.getElementById("startGameButton");
+const getNumberButton = document.querySelector("#getNumberButton");
+const startGameButton = document.querySelector("#startGameButton");
 
-const playerDiv = document.getElementById("playerDiv");
-const pcDiv = document.getElementById("pcDiv");
-const myNodelist = document.querySelectorAll("div.box");
+const playerDiv = document.querySelector("#playerDiv");
+const pcDiv = document.querySelector("#pcDiv");
+const boxDivlist = document.querySelectorAll("div.box");
 
 let bingoNumbers = [];
-let arrayNumbersHalf = [];
-let arrayNumbersAnotherHalf = [];
-let arrayOfNumbers = [];
+let bingoCard = [];
+let anotherBingoCard = [];
+let joinedCards = [];
 
-const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+const range = (start, stop, step) => {
+  return Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
+}
 
+const randomIntFromInterval = (min, max) => {
 
-const randomIntFromInterval = (min, max) => { 
-  
   let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNumber;
 }
@@ -30,6 +31,7 @@ const generateNumbers = () => {
   return arr;
 }
 
+//renombrar nombres de variables para que se entiendan
 const removeElement = (arr, val) => {
   for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] == val) {
@@ -39,52 +41,49 @@ const removeElement = (arr, val) => {
   return arr.length;
 };
 
-
+//renombrar variables para que se entiendan
 const startGame = () => {
   getNumberButton.disabled = false;
   numberDiv.innerHTML = " ";
   playerDiv.innerHTML = " ";
   pcDiv.innerHTML = " ";
-  bingoNumbers = range(1,90,1);
-  arrayNumbersHalf = generateNumbers();
-  arrayNumbersAnotherHalf = generateNumbers();
-  arrayOfNumbers = arrayNumbersHalf.concat(arrayNumbersAnotherHalf);
+  bingoNumbers = range(1, 90, 1);
+  bingoCard = generateNumbers();
+  anotherBingoCard = generateNumbers();
+  joinedCards = bingoCard.concat(anotherBingoCard);
 
-  for (let index in myNodelist) {
-    if (myNodelist[index].innerHTML) {
-      myNodelist[index].innerHTML = arrayOfNumbers[index];
-      myNodelist[index].style.color = "black";
-      myNodelist[index].style.textDecoration = "none";
+  for (let index in boxDivlist) {
+    if (boxDivlist[index].innerHTML) {
+      boxDivlist[index].innerHTML = joinedCards[index];
+      //boxDivlist[index].classList.add("number");
     }
   }
 }
 
 
 const pickNumber = () => {
-  //cada numero aleatorio solo puede salir una vez, si ya ha salido coger otro
-  const indice = randomIntFromInterval(1, bingoNumbers.length-1);
+  const indice = randomIntFromInterval(1, bingoNumbers.length - 1);
   let randomNumber = bingoNumbers[indice];
 
-    const numberDiv = document.getElementById("numberDiv");
-    numberDiv.innerHTML = randomNumber;
-  
-    removeElement(bingoNumbers, randomNumber);
+  const numberDiv = document.querySelector("#numberDiv");
+  numberDiv.innerHTML = randomNumber;
 
-  for (let element of myNodelist) {
+  removeElement(bingoNumbers, randomNumber);
+
+  for (let element of boxDivlist) {
     if (element.innerHTML == numberDiv.innerHTML) {
-      element.style.color = "red";
-      element.style.textDecoration = "line-through";
-      removeElement(arrayNumbersHalf, element.innerHTML);
-      removeElement(arrayNumbersAnotherHalf, element.innerHTML);
+      element.classList.add("line-through");
+      removeElement(bingoCard, element.innerHTML);
+      removeElement(anotherBingoCard, element.innerHTML);
 
-      if (arrayNumbersHalf.length == 0 && arrayNumbersAnotherHalf.length == 0) {
+      if (bingoCard.length == 0 && anotherBingoCard.length == 0) {
         pcDiv.innerHTML = "Empate";
         playerDiv.innerHTML = "Empate";
         getNumberButton.disabled = true;
-      } else if (arrayNumbersAnotherHalf.length == 0) {
+      } else if (anotherBingoCard.length == 0) {
         pcDiv.innerHTML = "Ganador PC";
         getNumberButton.disabled = true;
-      } else if (arrayNumbersHalf.length == 0) {
+      } else if (bingoCard.length == 0) {
         playerDiv.innerHTML = "Ganador Jugador";
         getNumberButton.disabled = true;
       }
